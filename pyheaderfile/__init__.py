2d5ac5b4fe2e8037d309b7ee55b273534eb926ad
Diogo <diogo@medksaude.com.br>
2014-12-29 12:17:52 -0200

    only valid kwargs are passed to classes

--- pyheaderfile/__init__.py
+++ pyheaderfile/__init__.py
@@ -529,7 +529,7 @@ class Ods(PyHeaderSheet):
 
 def guess_type(filename,**kwargs):
     """ Utility function to call classes based on filename extension.
-    Just usefull if you are reading the file and don't know file extension.
+    Just usefull if you are reading the file and don't know file extension. You can pass kwargs and these args are passed to class only if they are used in class.
     """
     import os
 
@@ -538,7 +538,14 @@ def guess_type(filename,**kwargs):
             '.xlsx': Xlsx,
             '.csv': Csv}
     if extension and case.get(extension.lower()):
-        return case.get(extension.lower())(filename, **kwargs)
+        low_extension = extension.lower()
+        new_kwargs = dict()
+        class_name = case.get(low_extension)
+        class_kwargs = class_name.__init__.func_code.co_names
+        for kwarg in kwargs:
+            if kwarg in class_kwargs:
+                new_kwargs[kwarg] = kwargs[kwarg]
+        return case.get(low_extension)(filename, **new_kwargs)
     else:
         raise Exception('No extension found')
 
