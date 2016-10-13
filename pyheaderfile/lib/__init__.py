0bbc92d8bfc72b9f0f6f561d2b728de96e65981f
Diogo <diogo.mvieira@gmail.com>
2016-10-13 18:24:32 -0300

    Update __init__.py

--- pyheaderfile/lib/__init__.py
+++ pyheaderfile/lib/__init__.py
@@ -3,8 +3,8 @@
 
 __all__ = ['Csv', 'Xls', 'Xlsx', 'guess_type']
 
-from headercsv import *
-from excel import *
-from libreoffice import *
-from drive import *
-from utils import *
+from pyheaderfile.lib.headercsv import *
+from pyheaderfile.lib.excel import *
+from pyheaderfile.lib.libreoffice import *
+from pyheaderfile.lib.drive import *
+from pyheaderfile.lib.utils import *
